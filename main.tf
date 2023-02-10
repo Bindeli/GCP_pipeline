1563c2c7c8df7d8c2271ecfb4876e308f0f66ebb
LucasBindeli <lucasbin2204@gmail.com>
2023-02-10 16:51:35 -0300

    creatingbucket

--- main.tf
+++ main.tf
@@ -7,83 +7,80 @@
 
 # chama o modulo
 
-module "bigquery-dataset-gasolina"" {
-    source = ".modules\bigquery"
-    dataset_id              =   "gasolina-brasil"
-    dataset_name            =   "gasolina-brasil"
-    description             =   "Dataset a respeito do historico de preços da gasolina do Brasil a partir de 2004"
-    project_id              =   var.project_id
-    location                =   var.region
-    deletion_contents_on_destroy = true 
-    deletion_protection = false
-    access = [ #definições de acesso
-        {
-            role = "OWNER"
-            special_group = "projectOwners"
+module "bigquery-dataset-gasolina" {
+  source  = "./modules/bigquery"
+  dataset_id                  = "gasolina_brasil"
+  dataset_name                = "gasolina_brasil"
+  description                 = "Dataset a respeito do histórico de preços da Gasolina no Brasil a partir de 2004"
+  project_id                  = var.project_id
+  location                    = var.region
+  delete_contents_on_destroy  = true
+  deletion_protection = false
+  access = [
+    {
+      role = "OWNER"
+      special_group = "projectOwners"
+    },
+    {
+      role = "READER"
+      special_group = "projectReaders"
+    },
+    {
+      role = "WRITER"
+      special_group = "projectWriters"
+    }
+  ]
+  tables=[
+    {
+        table_id           = "tb_historico_combustivel_brasil",
+        description        = "Tabela com as informacoes de preço do combustível ao longo dos anos"
+        time_partitioning  = {
+          type                     = "DAY",
+          field                    = "data",
+          require_partition_filter = false,
+          expiration_ms            = null
         },
-        {
-            role = "READER"
-            special_group = "projectReaders"
+        range_partitioning = null,
+        expiration_time = null,
+        clustering      = ["produto","regiao_sigla", "estado_sigla"],
+        labels          = {
+          name    = "stack_data_pipeline"
+          project  = "gasolina"
         },
-        {
-            role = "WRITER"
-            special_group = "projectWriters"
-        }
-    ]
-
-    tables = [  # onde criamos as tabelas
-        {
-            table_id = "tb_historico_combustivel_brasil"
-            description = "Tabela com as informacoes de preço de combustivel ao longo dos anos"
-            time_partitioning = {
-                type = "DAY",
-                field = "data",
-                require_partition_filter = false,
-                expiration_ms = null
-            },
-            range_partitioning = null,
-            expiration_time = null,
-            clustering = ["produto","regiao_sigla","estado_sigla"],
-            labels = { # para trackear onde esta consumindo na sua área
-                name = "stack_data_pipeline"
-                project = "gasolina"
-            },
-            deletion_protection = true 
-            schema = file(".\bigquery\schema\gasolina_brasil\tb_historico_combustivel_brasil.json")
-
-        }
-    ]
-  
+        deletion_protection = true
+        schema = file("./bigquery/schema/gasolina_brasil/tb_historico_combustivel_brasil.json")
+    }
+  ]
 }
 
-module "bucket-raw"{
-    source = ".\modules\gcs"
+module "bucket-raw" {
+  source  = "./modules/gcs"
 
-    name       = "data-pipeline-lucasbindeli-raw"
-    project_id = var.project_id
-    location   = var.region
-}
-
-module "bucket-curated" {
-    source = ".\modules\gcs"
-    name       = "data-pipeline-lucasbindeli-curated"
-    project_id = var.project_id
-    location   = var.region
-}
-
-module "bucket-psypark-tmp" {
-    source = ".\modules\gcs"
-    name       = "data-pipeline-lucasbindeli-pyspark-tmp"
-    project_id = var.project_id
-    location   = var.region
-}
-
-module "bucket-pyspark-code" {
-  source  = ".\modules\gcs"
-
-  name       = "data-pipeline-lucasbindeli-pyspark-code"
+  name       = "data-pipeline-lucasbindeli-brasil-raw"
   project_id = var.project_id
   location   = var.region
 }
 
+module "bucket-curated" {
+  source  = "./modules/gcs"
 
+  name       = "data-pipeline-lucasbindeli-brasil-curated"
+  project_id = var.project_id
+  location   = var.region
+}
+
+module "bucket-pyspark-tmp" {
+  source  = "./modules/gcs"
+
+  name       = "data-pipeline-lucasbindeli-brasil-pyspark-tmp"
+  project_id = var.project_id
+  location   = var.region
+}
+
+module "bucket-pyspark-code" {
+  source  = "./modules/gcs"
+
+  name       = "data-pipeline-lucasbindeli-brasil-pyspark-code"
+  project_id = var.project_id
+  location   = var.region
+}
